e9a9880d710ba6868c7f373a9316f53ed2a567df
dadosdidaticos <marcosvcc@poli.ufrj.br>
2022-08-25 01:04:18 -0300

    Adição do jogo,contagens e outras adequações

--- javascript.js
+++ javascript.js
@@ -9,21 +9,55 @@ function playRound(playerSelection,computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     winningList = ["rock-scissors","paper-rock","scissors-paper"]
     roundPlay = playerSelection+"-"+computerSelection
-    while (choice_arr.includes(playerSelection)==false){
-        playerSelection = prompt("player has chosen: "+playerSelection+".\nThis is not a valid entry. Please try again:")
-        playerSelection = playerSelection.toLowerCase()
-    }
     if (playerSelection==computerSelection){
-        return alert(`player has chosen: ${playerSelection}.\ncomputer has chosen: ${computerSelection}.\nThis round is drawn!`)
+        return "draw"
     }else if (winningList.includes(roundPlay)){
-        return alert(`player has chosen: ${playerSelection}.\ncomputer has chosen: ${computerSelection}.\nPlayer Wins!`)
+        return "player"
     }else{
-        return alert(`player has chosen: ${playerSelection}.\ncomputer has chosen: ${computerSelection}.\nComputer Wins!`)
+        return "computer"
     }
-
 }
-/*receber input do player*/
-var playerSelection = prompt("Select Rock, Paper or Scissors:")
-/*receber input do computador*/
-var computerSelection = getComputerChoice()
-console.log(playRound(playerSelection,computerSelection))
+
+
+
+/*game itself: its a best of five. First one to get 5 points, wins*/
+function game() {
+    /*no início do jogo, temos:*/
+    let roundNumber = 1
+    let playerScore = 0
+    let computerScore = 0
+
+    while (true){
+        /*receber input do player*/
+        var playerSelection = prompt(`Round ${roundNumber}!\nSelect Rock, Paper or Scissors:`)
+        playerSelection = playerSelection.toLowerCase()
+        while (choice_arr.includes(playerSelection)==false){
+            playerSelection = prompt("player has chosen: "+playerSelection+".\nThis is not a valid entry. Please try again:")
+            playerSelection = playerSelection.toLowerCase()
+        }
+        /*receber input do computador*/
+        var computerSelection = getComputerChoice()
+        /*obter vencedor e alertar msg*/
+        if (playRound(playerSelection,computerSelection)==="draw") {
+            alert(`player has chosen: ${playerSelection}.\ncomputer has chosen: ${computerSelection}.\nThis round is drawn!`+
+            `\nThe Score of the Game is: player: ${playerScore}, computer: ${computerScore}`)
+            roundNumber+=1
+        }else if (playRound(playerSelection,computerSelection)==="player") {
+            playerScore+=1
+            alert(`player has chosen: ${playerSelection}.\ncomputer has chosen: ${computerSelection}.\nplayer Wins round ${roundNumber}!`+
+            `\nThe Score of the Game is: player: ${playerScore}, computer: ${computerScore}`)
+            roundNumber+=1
+        }else if (playRound(playerSelection,computerSelection)==="computer") {
+            computerScore+=1
+            alert(`player has chosen: ${playerSelection}.\ncomputer has chosen: ${computerSelection}.\nComputer Wins round ${roundNumber}!`+
+            `\nThe Score of the Game is: player: ${playerScore}, computer: ${computerScore}`)
+            roundNumber+=1
+        }
+        /*Checagem de término de jogo*/
+        if (playerScore == 3){
+            return alert("Player Wins!")
+        }else if (computerScore == 3){
+            return alert("Computer Wins!")
+        }
+    }
+}
